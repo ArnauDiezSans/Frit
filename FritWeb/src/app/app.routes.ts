805aa838ebc4bbc17f87cf6d84861cb723a07b21
ArnauDiezSans <naudelsud@hotmail.com>
2026-06-02 13:51:55 +0200

    fix

--- FritWeb/src/app/app.routes.ts
+++ FritWeb/src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/auth/auth.guard';
 import { LoginPageComponent } from './features/auth/login-page.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 import { PartidasPageComponent } from './features/partidas/partidas-page.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
-  {
-    path: 'app/partidas',
-    component: PartidasPageComponent,
-    canActivate: [authGuard]
-  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'app/partidas', component: PartidasPageComponent, canActivate: [authGuard] },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: '**', redirectTo: 'login' }
 ];
